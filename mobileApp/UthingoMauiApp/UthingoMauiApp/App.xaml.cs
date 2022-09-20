84928c94af194ef22c3bb4f8a7be6b6121142e95
Lebogang Madise <lebo.mokgaetsi@gmail.com>
2022-09-20 12:28:46 +0200

    Intial commit of MAUI version 1 App

---
+++ mobileApp/UthingoMauiApp/UthingoMauiApp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace UthingoMauiApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ mobileApp/UthingoMauiApp/UthingoMauiApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace UthingoMauiApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
